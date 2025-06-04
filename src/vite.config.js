5850c89cbe56c06e34cf659ce9a0daf769f6b686
Gurkamal Singh <Gurkamalin@gmail.com>
2025-06-04 09:34:04 +0530

    Update entire project with latest changes

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:"/vitaplates/",
+  base: "/vitaplates/",
   plugins: [react()],
+  build: {
+    outDir: 'dish'  // Add this line to output build to 'dish'
+  },
   server: {
     proxy: {
       '/spoonacular': {
